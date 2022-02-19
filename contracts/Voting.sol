33c89f935f722c0b389ac1526c3b39127b5844bd
codecoco <isawmy@nate.com>
2022-02-19 18:17:45 +0900

    후보자 코드 수정

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,6 +5,7 @@ contract Voting {
   
   // 구조체
   struct Candidate {
+    uint256 number;
     string name;
     string party;
   }
@@ -13,8 +14,15 @@ contract Voting {
   Candidate[] public candidates;
 
   // 후보자 등록 함수
-  function registerCandidate(string memory _name, string memory _party) public {
-    candidates.push(Candidate(_name, _party));
+  function registerCandidate (uint256 _number, string memory _name, string memory _party) public {
+    candidates.push(Candidate(_number, _name, _party));
+  }
+
+  // 후보자 정보 조회
+  function getCandidates(uint256 _number) public view returns (uint256 getNumber, string memory getName, string memory getParty) {
+    getNumber = candidates[_number].number;
+    getName = candidates[_number].name;
+    getParty = candidates[_number].party;
   }
 
 }
--- test/voting.test.js
+++ test/voting.test.js
@@ -1,5 +1,29 @@
 const Voting = artifacts.require('Voting');
 
-contract('Voting', function () {
-  console.log(1);
+
+//[deployer,user1,user2] : ganache-cli에서 생성한 주소가 차례대로 들어옴.
+contract('Voting', function ([deployer, user1, user2]) {
+
+  let voting;
+
+  // 테스트용 배포
+  // new(): 빈칸이면 무조건 0번이 배포자가 됨.
+  beforeEach(async () => {
+    //console.log('beforeEach');
+    voting = await Voting.new();
+  })
+
+  describe('Candidate', function () {
+
+    it.only('register candidate', async () => {
+
+      await voting.registerCandidate(0, 'coco', 'happy')
+      await voting.registerCandidate(1, 'blackStone', 'happy')
+
+      console.log(await voting.getCandidates(0));
+      console.log('/////////////////////////////');
+      console.log(await voting.getCandidates(1));
+
+    })
+  })
 })
